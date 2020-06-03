382e7be24e16b3f61ddcb7ce86ee25a16df20e8c
Ahmed Hashim <ahmedhashim_2000@hotmail.com>
2020-06-03 16:19:27 +0500

    New App.Js udpate
    after error rectification

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          </br> 
-          Updated by Ahmed Hashim 
+           Updated by Ahmed Hashim 
         </p>
         <a
           className="App-link"
